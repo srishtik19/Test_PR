cb529e01bfe012f8d8ffb923c3193261ff7dc7c6
Srishti KHATRI <srkhatri@axway.com>
2026-03-25 11:13:42 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {{
   return ((
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" xml:space={}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
